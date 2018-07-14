026ed58b11462f144d90ec5a015751a7fdd26e4c
Jannik <Jannik.Feuerhahn@gmx.de>
2018-07-14 23:35:04 +0200

    fixed transports

--- server/src/socketserver.ts
+++ server/src/socketserver.ts
@@ -4,14 +4,20 @@
  */
 import * as socketio from "socket.io";
 import config from "./util/config";
-import Webserver from "./webserver";
 import { Server } from "http";
+import Log from "./util/log";
+
+export interface SocketDelegate {
+  onConnection(socket: socketio.Socket);
+  onMessage(socket: socketio.Socket, topic: String, data: {});
+}
 
 export default class SocketServer {
 
+  public delegate: SocketDelegate;
   public io: socketio.Server;
   private httpServer: Server;
-  
+
   constructor(httpServer: Server) {
     this.httpServer = httpServer;
   }
@@ -20,5 +26,6 @@ export default class SocketServer {
     this.io = socketio(this.httpServer, {
       transports: config.transports
     });
+    this.io.on("connection", (client) => { Log.info("Someone Connected"); })
   }
 };
