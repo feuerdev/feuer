aa698aa20f5939f535a824f8dfa3b06b0cf41584
Jannik <Jannik.Feuerhahn@gmx.de>
2018-07-21 16:01:52 +0200

    Updated Vector utilities

--- server/src/util/vector2.ts
+++ server/src/util/vector2.ts
@@ -1,24 +1,51 @@
+import Vector3 from "./vector3";
+
 export default class Vector2 {
+ 
 
-  private x:number;
-  private y:number;
+  public x: number = 0;
+  public y: number = 0;
 
-  constructor(p1:{x:number,y:number,z:number}, p2:{x:number,y:number,z:number}) {
-    if(p1 && p2) {
-      this.x = p2.x - p1.x;
-      this.y = p2.y - p1.y;
+  constructor(); //Constructor 1
+  constructor(angle: number); //Constructor 2
+  constructor(p1: { x: number, y: number }, p2: { x: number, y: number }); //Constructor 3
+  constructor(x: number, y: number); //Constructor 4
+  constructor(arg1?: { x: number, y: number } | number, arg2?: { x: number, y: number } | number) {
+    if (arg1 && arg2) {
+      if (typeof arg1 === "number" && typeof arg2 === "number") {
+        //Constructor 4
+        this.x = arg1;
+        this.y = arg2;
+      } else if (typeof arg1 === "object" && typeof arg2 === "object") {
+        //Constructor 3
+        this.x = arg2.x - arg1.x;
+        this.y = arg2.y - arg1.y;
+      }
+    } else if (arg1) {
+      //Constructor 2
+      if(typeof arg1 === "number") {
+        this.x = Math.cos(arg1);
+        this.y = Math.sin(arg1);
+        this.normalize();
+      }
+    } else {
+      //Constructor 1
     }
   }
 
-  public length():number {
-    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) );
+  public length(): number {
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
   }
 
   public normalize(): void {
     const length = this.length();
-    if(length != 0) {
-        this.x /= length;
-        this.y /= length;
+    if (length != 0) {
+      this.x /= length;
+      this.y /= length;
     }
+  }
+
+  public toVector3(): Vector3 {
+    return new Vector3(this.x, this.y, 0);  
   }
 }
--- server/src/util/vector3.ts
+++ server/src/util/vector3.ts
@@ -1,28 +1,59 @@
 export default class Vector3 {
+  public x: number = 0;
+  public y: number = 0;
+  public z: number = 0;
 
-  public x:number;
-  public y:number;
-  public z:number;
-
-  constructor();
-  constructor(p1?:{x:number,y:number,z:number}, p2?:{x:number,y:number,z:number}) {
-    if(p1 && p2) {
-      this.x = p2.x - p1.x;
-      this.y = p2.y - p1.y;
-      this.z = p2.z - p1.z;
+  constructor(); //Constructor 1
+  constructor(angleHorizontal: number, angleVertical: number); //Constructor 2
+  constructor(p1: { x: number, y: number, z: number }, p2: { x: number, y: number, z: number }); //Constructor 3
+  constructor(x: number, y: number, z: number);
+  constructor(arg1?: { x: number, y: number, z: number } | number, arg2?: { x: number, y: number, z: number } | number, arg3?: number) {
+    if (arg1 && arg2 && arg3) {
+      if(typeof arg1 === "number" && typeof arg2 === "number" && typeof arg3 === "number") {
+        this.x = arg1;
+        this.y = arg2;
+        this.z = arg3;
+      }
+    } else if(arg1 && arg2) {
+      if (typeof arg1 === "number" && typeof arg2 === "number") {
+        //Constructor 2
+        this.x = Math.cos(arg1) * Math.cos(arg2);
+        this.y = Math.sin(arg1) * Math.cos(arg2);
+        this.z = Math.sin(arg2);
+        this.normalize();
+      } else if (typeof arg1 === "object" && typeof arg2 === "object") {
+        //Constructor 3
+        this.x = arg2.x - arg1.x;
+        this.y = arg2.y - arg1.y;
+        this.z = arg2.z - arg1.z;
+      }
+    } else {
+      //Constructor 1
     }
   }
 
-  public length():number {
+  public length(): number {
     return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2));
   }
 
   public normalize(): void {
     const length = this.length();
-    if(length != 0) {
-        this.x /= length;
-        this.y /= length;
-        this.z /= length;
+    if (length != 0) {
+      this.x /= length;
+      this.y /= length;
+      this.z /= length;
     }
+  }
+
+  public add(vector: Vector3): void {
+    this.x += vector.x;
+    this.y += vector.y;
+    this.z += vector.z;
+  }
+  
+  public multiply(factor: number): void {
+    this.x *= factor;
+    this.y *= factor;
+    this.z *= factor;
   }
 }
