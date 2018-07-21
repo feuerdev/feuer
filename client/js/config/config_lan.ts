2bc4cba758f48f51d3e8f75647fab7ef05fbac3c
Jannik <Jannik.Feuerhahn@gmx.de>
2018-07-22 01:36:46 +0200

    added lan config

---
+++ client/js/config/config_lan.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Jannik on 04.09.2016.
+ */
+// @ts-ignore
+const config = {
+    ip: "192.168.178.23:8000",
+    transports: ["websocket"],
+    updaterate: 60,
+    log_level: "debug"
+};
