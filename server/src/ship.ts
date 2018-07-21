864a12192d8df1ac4cb4f70dd524db3bfad224bb
Jannik <Jannik.Feuerhahn@gmx.de>
2018-07-22 01:23:11 +0200

    added owner of ship

--- server/src/ship.ts
+++ server/src/ship.ts
@@ -3,16 +3,22 @@ import Vector2 from "./util/vector2";
 
 export default class Ship {
 
+  public owner: string;
+
+  constructor(owner:string) {
+    this.owner = owner;
+  }
+
   public pos:Vector2 = new Vector2();
   public orientation: number = 0;
 
   public rudderAngleActual: number = 0;
   public rudderAngleRequested: number = 0;
-  public turnSpeed: number = 0.2;
+  public turnSpeed: number = 0.3;
 
   public speed_actual:number = 0;
   public speed_requested: number = 0;
-  public acceleration: number = 0.05;
+  public acceleration: number = 0.01;
 
   public gun: Gun = new Gun();
 }
