250cc841fce457f0ae4b690e9d04c95dafb35362
Jannik <Jannik.Feuerhahn@gmx.de>
2018-11-19 13:06:50 +0100

    Registering is sending username to db

--- client/js/web/register.js
+++ client/js/web/register.js
@@ -1,5 +1,7 @@
 const form_register = document.getElementById("register");
 
+
+
 if (form_register) {
   form_register.addEventListener("submit", function (e) {
     e.preventDefault(); //stop form from submitting
@@ -10,14 +12,31 @@ if (form_register) {
 
     if (password === passwordRepeat) {
       firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(function (success) {
-          console.log("User successfully registered, redirecting");
-          success.updateProfile({
-            displayName: username
-          }).catch(err => {
-            console.log(err);
-          });
-          window.location.replace("/");
+        .then(function () {
+          console.log("User successfully registered, sending data to server");
+
+          function waitForCookie() {
+            console.log("checking if cookie is already set...");
+            if (document.cookie.indexOf("__session=") >= 0) {
+              console.log("cookie is set");
+              const xmlhttp = new XMLHttpRequest();
+              xmlhttp.open("POST", "/register");
+              xmlhttp.setRequestHeader("Content-Type", "application/json");
+              xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                  console.log("Data sent to server, redirecting");
+                  window.location.replace("/");
+                }
+              };
+              xmlhttp.send(JSON.stringify({
+                username: username
+              }));
+            } else {
+              console.log("cookie is not set yet. trying again...");
+              setTimeout(waitForCookie, 200);
+            }
+          }
+          waitForCookie();
         })
         .catch(function (error) {
           if (error) {
