3dfeef774edaea99e12e1ccdb27c98d133dc0a40
Jannik <Jannik.Feuerhahn@gmx.de>
2018-11-20 00:14:57 +0100

    leaderboard!

--- server/src/web/routes/home.ts
+++ server/src/web/routes/home.ts
@@ -1,10 +1,31 @@
 import * as express from "express";
+import * as db from "../../util/db";
 const router = express.Router();
 
 //Route: /
 
-router.get("/", function(req, res) {
-  res.render("home", {username: req.user.game.username});
+router.get("/", function (req, res) {
+
+  db.query("SELECT users.username as 'killer', COUNT(*) AS 'kills' FROM kills JOIN users ON kills.killer=users.uid GROUP BY killer", function (error, results, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      let leaderboardItems = [];
+      for (let i = 0; i < results.length; i++) {
+        const result = results[i];
+        leaderboardItems.push({
+          killer: result.killer,
+          kills: result.kills
+        });
+      }
+      res.render("home", {
+        username: req.user.game.username,
+        leaderboardItems: leaderboardItems
+      });
+    }
+  });
+
+
 });
 
 
