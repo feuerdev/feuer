16f079eb532e238b85de2d52fabeb243d8dff015
Jannik <Jannik.Feuerhahn@gmx.de>
2018-11-22 21:12:56 +0100

    gastlogin

--- client/js/web/login.js
+++ client/js/web/login.js
@@ -21,3 +21,37 @@ if (form_login) {
 
     });
 }
+
+function onGuestClicked() {
+    firebase.auth().signInAnonymously()
+            .then(function (success) {
+                function waitForCookie() {
+                    console.log("checking if cookie is already set...");
+                    if (document.cookie.indexOf("__session=") >= 0) {
+                      console.log("cookie is set");
+                      const xmlhttp = new XMLHttpRequest();
+                      xmlhttp.open("POST", "/register");
+                      xmlhttp.setRequestHeader("Content-Type", "application/json");
+                      xmlhttp.onreadystatechange = function () {
+                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                          console.log("Data sent to server, redirecting");
+                          window.location.replace("/");
+                        }
+                      };
+                      xmlhttp.send(JSON.stringify({
+                        username: "Gast"
+                      }));
+                    } else {
+                      console.log("cookie is not set yet. trying again...");
+                      setTimeout(waitForCookie, 200);
+                    }
+                  }
+                  waitForCookie();
+            })
+            .catch(function (error) {
+                if (error) {
+                    console.log(error.code);
+                    console.log(error.message);
+                } else {}
+            });
+}
