f9884e2ab753a17a4c45b9bb134133f9d0a91fa5
Jannik <Jannik.Feuerhahn@gmx.de>
2019-09-22 23:55:42 +0200

    prepared configs for hexes

--- client/js/config/config_debug.ts
+++ client/js/config/config_debug.ts
@@ -9,9 +9,8 @@ const config = {
     log_level: "debug",
     zoom_min: 0.25,
     zoom_max: 4,
-    gravity: -0.01,
-    input_factor: 0.4,
     input_time_threshold: 150,
-    waypoint_distance_threshold: 20,
-    click_distance_threshold: 3
+    click_distance_threshold: 3,
+    hex_width: 100,
+    hex_height: 100
 };
