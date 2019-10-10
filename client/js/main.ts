b90198b3f7120456ef87290b02d43e36084d7c97
Jannik Feuerhahn <jannik@Janniks-MBP.fritz.box>
2019-10-10 14:26:22 +0200

    created game class again

--- client/js/main.ts
+++ client/js/main.ts
@@ -5,51 +5,56 @@ import * as $ from "./lib/jquery-3.1.1.min";
 import Log from "./util/log";
 import Vector2 from "../../shared/vector2";
 import Gameloop from "../../shared/gameloop";
-import { Layout } from "../../shared/hex";
+import Hex, { Layout } from "../../shared/hex";
 import Renderer from "./renderer";
-import Input from "./input";
+import Input, { InputListener } from "./input";
 import Connection, { ConnectionListener } from "./connection";
 import { Socket } from "socket.io";
 
 declare const config;
 
+class Game implements InputListener, ConnectionListener {
+    private canvas_map = $("#canvas-map");
+    private canvas_fow = $("#canvas-fow");
+    private canvas_entities = $("#canvas-entities");
+    private div_container = $("#canvas-container");
+    private div_debug = $("#debug");
+    private gameloop:Gameloop;
+    private connection:Connection;
+    private layout:Layout;
+    private renderer:Renderer;
+    private input:Input;
+
+    constructor() {
+        this.gameloop = new Gameloop(Gameloop.requestAnimationFrameWrapper, config.updaterate, config.netrate)
+        this.connection = new Connection(config.ip, config.transports);
+        this.layout = new Layout(Layout.pointy, new Vector2(config.hex_width, config.hex_height), new Vector2(0, 0));
+        this.renderer = new Renderer(this.div_container, this.canvas_map, this.canvas_fow, this.canvas_entities, this.div_debug, this.layout);
+        this.input = new Input(this.canvas_entities, this.layout);
+
+        this.gameloop.addListener(this.input);
+        this.gameloop.addListener(this.renderer);
+        this.input.addListener(this.renderer);
+        this.input.addListener(this);
+        this.connection.addListener(this);
+        this.gameloop.run();
+
+        Log.info("Client ready");
+    }
+    onHexSelected(hex: Hex) {
+        this.connection.send("request movement", hex);
+    }
+    onConnected(socket: Socket) {
+        Log.info("Connected");
+    }
+    onDisconnected(socket: Socket) {
+        Log.info("Disonnected");
+    }
+    onSetup(socket: Socket) {
+        socket.on("gamestate world", (data) => this.renderer.setWorld(data));
+    }
+}
+
 $(document).ready(function () {
-    let canvas_map = $("#canvas-map");
-    let canvas_fow = $("#canvas-fow");
-    let canvas_entities = $("#canvas-entities");
-    let div_container = $("#canvas-container");
-    let div_debug = $("#debug");
-
-    let gameloop = new Gameloop(Gameloop.requestAnimationFrameWrapper, config.updaterate, config.netrate)
-    let connection = new Connection(config.ip, config.transports);
-    let layout:Layout = new Layout(Layout.pointy, new Vector2(config.hex_width, config.hex_height), new Vector2(0,0));
-    let renderer = new Renderer(div_container, canvas_map, canvas_fow, canvas_entities, div_debug, layout);
-    let input = new Input(canvas_entities, layout);
-
-    gameloop.addListener(input);
-    gameloop.addListener(renderer);
-    input.addListener(renderer);
-    connection.addListener(new class a implements ConnectionListener {
-
-        constructor() {
-            console.log("Test");
-        }
-
-        onConnected(socket:Socket) {
-            Log.info("Connected");
-        }
-        onDisconnected(socket:Socket) {
-            Log.info("Disonnected");
-        }
-        onSetup(socket:Socket) {            
-            socket.on("gamestate world", (data) => this.onGamestateWorld(data));
-        }
-        onGamestateWorld(data: any): void {
-            renderer.setWorld(data);
-        }
-    });
-
-    gameloop.run();
-
-    Log.info("Client ready");
+    new Game();
 });
