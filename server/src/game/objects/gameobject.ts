05f50429bb0006a399b0bb66029353477ae486e2
Jannik <Jannik.Feuerhahn@gmx.de>
2019-10-10 11:45:38 +0200

    gameobject have ids

--- server/src/game/objects/gameobject.ts
+++ server/src/game/objects/gameobject.ts
@@ -1,7 +1,10 @@
 export default abstract class GameObject {
-  protected owner:string; 
+  protected owner:string;
+  protected id:number;
+  private static idcount:number = 0;
 
   constructor(owner) {
     this.owner = owner;
+    this.id = GameObject.idcount++;
   }
 }
