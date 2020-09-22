2cb1381dfa9fb6dafabf239e6b224da1a80e806e
Jannik Feuerhahn <jannik@feuer.dev>
2020-09-23 01:43:39 +0800

    added helper methods to ClientWorld

--- client/js/clientworld.ts
+++ client/js/clientworld.ts
@@ -1,10 +1,32 @@
 import { Hashtable } from "../../shared/util";
 import PlayerRelation from "../../shared/relation";
+import Hex from "../../shared/hex";
 
 export default class ClientWorld {
   public tiles: any = {};
-  public armies: any[] = [];
+  public groups: any[] = [];
   public battles: any[] = [];
   public buildings: any[] = [];
   public playerRelations:Hashtable<PlayerRelation> = {};
+
+  getGroup(id:number):any|undefined {
+    this.groups.find(group => { return group.id === id});
+  }
+
+  getTile(hex:Hex):any|undefined {
+    return this.tiles[hex.hash()];
+  }
+
+  getTileWithHash(hash:string):any|undefined {
+    return this.tiles[hash];
+  }
+
+  getBattleById(id:number):any|undefined {
+    this.battles.find(battle => { return battle.id === id});
+  }
+
+  getBuilding(id:number):any|undefined {
+    this.buildings.find(building => { return building.id === id});
+  }
+  
 }
