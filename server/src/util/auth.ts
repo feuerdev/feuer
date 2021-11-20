ec39ecbf0eb3c40c638991aa8e52add369318365
Jannik Feuerhahn <jannik@feuer.dev>
2021-11-20 20:58:57 +0100

    fix: redirect auth routes to login on errors

--- server/src/util/auth.ts
+++ server/src/util/auth.ts
@@ -5,18 +5,21 @@ export async function isAuthenticated(req, res, next) {
   if (!req.cookies) {
     Log.warn("No cookies found")
     res.redirect("/login")
+    return
   }
 
   const idToken = req.cookies.__session
   if (!idToken) {
     Log.warn("No idToken found")
     res.redirect("/login")
+    return
   }
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken)
     req.user = decodedToken
     next()
+    return
   } catch (error) {
     //TODO: Fange hier nur den Token expired Error ab.
     res.redirect("/relogin")
@@ -26,11 +29,13 @@ export async function isAuthenticated(req, res, next) {
 export async function deserializeAuth(req, res, next) {
   if (!req.cookies) {
     Log.warn("No cookies found")
+    //TODO: should we stop here?
   }
 
   const idToken = req.cookies.__session
   if (!idToken) {
     Log.warn("No idToken found")
+    //TODO: should we stop here?
   }
 
   const decodedToken = await admin.auth().verifyIdToken(idToken)
