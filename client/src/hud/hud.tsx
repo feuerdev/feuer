16b6f17f94ca96e457f3a44ad6b09a1b647cbd1c
Jannik Feuerhahn <jannik.feuerhahn@eng-its.de>
2022-10-19 20:32:51 +0200

    fix: improve hud data flow

--- client/src/hud/hud.tsx
+++ client/src/hud/hud.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import EventBus from "../eventbus"
 
-import { getTileById } from "../../../shared/objectutil"
+import { getTileById, getTileByPos } from "../../../shared/objectutil"
 import GroupInformation from "./group-info"
 
 export default function Hud() {
@@ -11,7 +11,6 @@ export default function Hud() {
   React.useEffect(() => {
     // Subscribe to relevant events
     EventBus.shared().on("selection", ({ detail }) => {
-      setSelectionType(detail.type)
       switch (detail.type) {
         case 0:
           setselectionData(null)
@@ -26,6 +25,7 @@ export default function Hud() {
           setselectionData(window.game.world.buildings[detail.selectedId])
           break
       }
+      setSelectionType(detail.type)
     })
 
     EventBus.shared().on("deselection", () => {
@@ -39,7 +39,7 @@ export default function Hud() {
       <div id="top-bar" className="w-full bg-slate-900 fixed top-0 flex"></div>
       <div id="bottom-bar" className="w-full h-1/5 bg-slate-900 fixed bottom-0">
         {selectionType == 0 && <div>Nix</div>}
-        {selectionType == 1 && <GroupInformation group={selectionData} />}
+        {selectionType == 1 && <GroupInformation group={selectionData} tile={getTileByPos(selectionData.pos, window.game.world.tiles)} />}
         {selectionType == 2 && <div>Tile</div>}
         {selectionType == 3 && <div>Building</div>}
         {/* <code>{JSON.stringify(selectionData)}</code> */}
