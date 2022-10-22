56268f1dbaf42179671a0494515dc7b8c8aa69f9
Jannik Feuerhahn <jannik.feuerhahn@eng-its.de>
2022-10-22 21:42:25 +0200

    fix: add shared code

---
+++ shared/vector2.ts
@@ -0,0 +1,30 @@
+import Vector3 from "./vector3"
+
+export default interface Vector2 {
+  x: number
+  y: number
+}
+
+export function create(x: number, y: number): Vector2 {
+  return { x, y }
+}
+
+export function length(): number {
+  return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+}
+
+export function normalize(): void {
+  const length = this.length()
+  if (length != 0) {
+    this.x /= length
+    this.y /= length
+  }
+}
+
+export function add(vector: Vector2): Vector2 {
+  return { x: this.x + vector.x, y: this.y + vector.y }
+}
+
+export function toVector3(): Vector3 {
+  return { x: this.x, y: this.y, z: 0 }
+}
