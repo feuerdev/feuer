8cf9a42a72feeb4f9728ed294f89c8cbc2dd358b
Jannik Feuerhahn <jannik@feuer.dev>
2023-06-25 21:22:55 +0200

    fix: simplify server entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,30 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
 import "./index.css"
+import { AuthProvider, useAuthContext } from "./components/AuthContext"
+import Login from "./components/Login"
+import { SocketProvider } from "./components/SocketContext"
+import Game from "./components/Game"
+
+const Main = () => {
+  const { user } = useAuthContext()
+
+  if (!user) {
+    return <Login />
+  }
+
+  return (
+    <SocketProvider>
+      <Game />
+    </SocketProvider>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <AuthProvider>
+        <Main />
+      </AuthProvider>
   </React.StrictMode>
 )
+
